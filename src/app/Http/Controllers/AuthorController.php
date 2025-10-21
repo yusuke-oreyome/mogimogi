112c528d914aa09fdf5b6fea94edbd3b9d5daa6e
yusuke <huka-7sutabeta1@i.softbank.jp>
2025-10-21 22:05:08 +0900

    first commit

---
+++ src/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    //
+}
---
+++ src/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [ContactController::class, 'index']);
+Route::post('/confirm', [ContactController::class, 'confirm']);
+Route::post('/thanks', [ContactController::class, 'store']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', [ContactController::class, 'admin']);
+    Route::get('/search', [ContactController::class, 'search']);
+    Route::post('/delete', [ContactController::class, 'destroy']);
+    Route::post('/export', [ContactController::class, 'export']);
+});
